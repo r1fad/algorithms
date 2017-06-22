a26222015d0707d3afe44163162d60fe229122c7
Rifad Lafir <rifadahmedlafir@gmail.com>
2017-06-22 17:51:28 +0300

    inital commit

---
+++ peakFinding_1D.py
@@ -0,0 +1,28 @@
+from random import randint
+def findPeak(aList):
+
+    print aList
+
+    length = len(aList)
+
+    midIndex = length/2
+
+    #if mid position less than element to its left look at left
+    #half of list
+    if aList[midIndex] < aList[midIndex-1]:
+        findPeak(aList[:midIndex-1])
+
+    #if mid positon less than element to its right look at right
+    #half of list
+    elif aList[midIndex] < aList[midIndex+1]:
+        findPeak(aList[midIndex+1:])
+
+    #else mid position is the peak
+    else:
+        return aList[midIndex]
+
+def genList():
+    return [randint(1,10) for i in range(10)]
+
+aList = genList()
+print findPeak(aList)
